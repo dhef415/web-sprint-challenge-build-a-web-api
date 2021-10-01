79cc5619c463f0ddcb5c548d3c38fa04661a2579
dhef415 <dhef415@gmail.com>
2021-10-01 15:32:44 -0700

    projects router and actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,75 @@
-// Write your "actions" router here!
+const express = require('express')
+const Actions = require('./actions-model')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(() => {
+            res.status(404).json({message: 'not found'})
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Actions.get(id)
+        .then(byId => {
+            if (!byId) {
+                res.status(404).json({message: 'not here'})
+            } else {
+                res.json(byId)
+            }
+        })
+        .catch(() => {
+            res.status(404).json({message: 'not here'})
+        })
+})
+
+router.post('/', (req, res) => {
+    Actions.insert(req.body)
+        .then(newPost => {
+            res.json(newPost)
+        })
+        .catch(() => {
+            res.status(400).json({message: ''})
+        })
+})
+
+router.put('/:id', (req,res) => {
+    const { id } = req.params
+    Actions.update(id, req.body)
+        .then(updates => {
+            if (!updates) {
+                res.status(400).json({message:'try again'})
+            } else {
+                res.json(updates)
+            }
+        })
+        .catch(() => {
+            res.status(400).json({message: 'try again'})
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    Actions.get(id)
+        .then(tbd => {
+            if (!id) {
+                res.status(404).json({message: 'not found'})
+            } else {
+                Actions.remove(id)
+                    .then(
+                        res.status(200).json(tbd)
+                    )
+                    .catch(() => {
+                        res.status(404).json({message: 'not found'})
+                        
+                    })
+            }
+        })
+})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,82 @@
-// Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(project => {
+            res.status(404).json(project)
+        })
+        .catch(res.status(404))
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Projects.get(id)
+        .then(byId => {
+            if (!byId) {
+                res.status(404).json({message: 'not found'})
+            } else {
+                res.status(200).json(byId)
+            }
+        })
+        .catch(res.status(500))
+})
+
+router.post('/', (req, res) => {
+    Projects.insert(req.body)
+        .then(newProj => {
+            res.status(201).json(newProj)
+        })
+        .catch(() => {
+            res.status(400).json({message: 'missing data'})
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    Projects.update(id, req.body)
+        .then(changes => {
+            if (!id) {
+                res.status(404).json({message: 'does not exist'})
+            } else {
+                res.status(200).json(changes)
+            }
+        })
+        .catch(() => {
+            res.status(400).json({message: 'nope'})
+        })
+})
+
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    Projects.get(id)
+        .then(toDelete => {
+            if (!id) {
+                res.status(404).json({message: 'no project with given id'})
+            } else {
+                Projects.remove(id)
+                    .then(
+                        res.status(200).json(toDelete)
+                    )
+                    .catch(err => {
+                        console.log(err)
+                    })
+            }
+        })
+        .catch(() => {
+            res.status(404).json({message: 'no'})
+        })
+})
+
+router.get('/:id/actions', (req, res) => {
+    const { id } = req.params
+    Projects.getProjectActions(id)
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(res.status(404))
+})
+
+module.exports = router
