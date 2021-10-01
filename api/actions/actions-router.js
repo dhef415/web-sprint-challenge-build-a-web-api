2a0b40dff4987889484d2290906f6f0ae9029924
dhef415 <dhef415@gmail.com>
2021-10-01 16:53:30 -0700

    As far as i got

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,74 +2,72 @@ const express = require('express')
 const Actions = require('./actions-model')
 const router = express.Router()
 
-
 router.get('/', (req, res) => {
-    Actions.get()
-        .then(action => {
-            res.status(200).json(action)
-        })
-        .catch(() => {
-            res.status(404).json({message: 'not found'})
-        })
+  Actions.get()
+    .then(action => {
+      res.status(200).json(action)
+    })
+    .catch(() => {
+      res.status(404).json({ message: 'not found' })
+    })
 })
 
 router.get('/:id', (req, res) => {
-    const { id } = req.params
-    Actions.get(id)
-        .then(byId => {
-            if (!byId) {
-                res.status(404).json({message: 'not here'})
-            } else {
-                res.json(byId)
-            }
-        })
-        .catch(() => {
-            res.status(404).json({message: 'not here'})
-        })
+  const { id } = req.params
+  Actions.get(id)
+    .then(byId => {
+      if (!byId) {
+        res.status(404).json({ message: 'not here' })
+      } else {
+        res.json(byId)
+      }
+    })
+    .catch(() => {
+      res.status(404).json({ message: 'not here' })
+    })
 })
 
 router.post('/', (req, res) => {
-    Actions.insert(req.body)
-        .then(newPost => {
-            res.json(newPost)
-        })
-        .catch(() => {
-            res.status(400).json({message: ''})
-        })
+  Actions.insert(req.body)
+    .then(newPost => {
+      res.json(newPost)
+    })
+    .catch(() => {
+      res.status(400).json({ message: '' })
+    })
 })
 
-router.put('/:id', (req,res) => {
-    const { id } = req.params
-    Actions.update(id, req.body)
-        .then(updates => {
-            if (!updates) {
-                res.status(400).json({message:'try again'})
-            } else {
-                res.json(updates)
-            }
-        })
-        .catch(() => {
-            res.status(400).json({message: 'try again'})
-        })
+router.put('/:id', (req, res) => {
+  const { id } = req.params
+  Actions.update(id, req.body)
+    .then(updates => {
+      if (!updates) {
+        res.status(400).json({ message: 'try again' })
+      } else {
+        res.json(updates)
+      }
+    })
+    .catch(() => {
+      res.status(400).json({ message: 'try again' })
+    })
 })
 
-router.delete('/:id', (req, res) => {
-    const { id } = req.params
-    Actions.get(id)
-        .then(tbd => {
-            if (!id) {
-                res.status(404).json({message: 'not found'})
-            } else {
-                Actions.remove(id)
-                    .then(
-                        res.status(200).json(tbd)
-                    )
-                    .catch(() => {
-                        res.status(404).json({message: 'not found'})
-                        
-                    })
-            }
-        })
+router.delete('/:id', async (req, res) => {
+  try {
+    const toBeDeleted = Actions.findById(req.params.id)
+    if (!toBeDeleted) {
+      res.status(404).json({
+        message: 'The user does not exist',
+      })
+    } else {
+      const deletedUser = await Actions.remove(toBeDeleted.id)
+      res.status(200).json(deletedUser)
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'The user could not be removed',
+    })
+  }
 })
 
 module.exports = router
